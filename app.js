9f2a54486ebfae0b8940bfe0a82799e3bd686b3e
connorwarme <connor.warme@gmail.com>
2024-03-07 18:00:19 -0700

    Add cors middleware for contact route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const nodemailer = require('nodemailer')
 const path = require('path')
 
@@ -6,9 +7,13 @@ const app = express()
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(3000)
+app.options('/contact', cors())
+app.post('/contact', cors(),  (req, res) => {
+  console.log('contact needs nodemailer implemented')
+})
+
+app.listen(3001)
