4cba5ff9a58c44162b249b7f021037363a76e063
Connor Warme <connor.warme@gmail.com>
2024-03-30 14:53:08 -0600

    Add temp mailer for connor contact

--- app.js
+++ app.js
@@ -80,6 +80,60 @@ app.post("/contact", cors(corsOptions), (req, res) => {
   main().catch(console.error);
   })
 
+  const corsOptionsC = {
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+  };
+  
+  app.options("/contactConnor", cors(corsOptionsC));
+  app.post("/contactConnor", cors(corsOptionsC), (req, res) => {
+    body('email').isEmail().normalizeEmail().escape()
+    body('message').trim().notEmpty().escape()
+  
+    const errors = validationResult(req)
+  
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() })
+      return
+    }
+  
+    const output = `
+      <p>You have a new contact request.</p>
+      <h3>Contact Details</h3>
+      <p>${req.body.email}</p>
+      <h3>Message Content</h3>
+      <p>${req.body.message}</p>
+    `;
+  
+    const transporterC = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.AUTH_C_USER,
+        pass: process.env.AUTH_C_PASS,
+      },
+    });
+    // still need to update these mail options (from, to)
+    // still need to set it up with my email, and with domain...
+    const mailOptionsC = {
+      from: '"Contact Connor" <contact@amitywarme.com>', // sender address
+      to: "connorwarme@gmail.com", // list of receivers
+      subject: "New Contact Request", // subject line
+      html: output, // html body
+    };
+      // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // send mail with defined transport object
+      const info = await transporterC.sendMail(mailOptionsC);
+    
+      console.log("Message sent: %s", info.messageId);
+      res.json({ contact: req.body })
+    }
+    
+    main().catch(console.error);
+    })
+
 // fly.io port is 8080
 const port = process.env.PORT || "8080";
 
